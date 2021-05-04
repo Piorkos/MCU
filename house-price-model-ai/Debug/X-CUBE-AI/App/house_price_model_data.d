656011ee25f83a4b5c423d213b477353a1326b77
Dominik Piórkowski <dominik.piorkowski@wp.pl>
2021-05-04 15:56:54 +0200

    removed unused/outdated folder

--- house-price-model-ai/Debug/X-CUBE-AI/App/house_price_model_data.d
+++
@@ -1,8 +0,0 @@
-X-CUBE-AI/App/house_price_model_data.o: \
- ../X-CUBE-AI/App/house_price_model_data.c \
- ../X-CUBE-AI/App/house_price_model_data.h \
- ../Middlewares/ST/AI/Inc/ai_platform.h
-
-../X-CUBE-AI/App/house_price_model_data.h:
-
-../Middlewares/ST/AI/Inc/ai_platform.h:
